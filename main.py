a397198db529eddfb692341f8fd9fff17e17c6e1
jeeva <jeevasathappan24@gmail.com>
2024-11-02 16:36:20 +0530

    Final commit with Minor issue fixes

--- main.py
+++ main.py
@@ -10,26 +10,31 @@ days = st.slider('Forecast days:', min_value=1, max_value=5,
 option = st.selectbox('Select data to view', ('Temperature', 'Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
 
-if place:
-    filtered_data = get_data(place, days)
-    if option == 'Temperature':
-        temperatures = [dict['main']['temp'] for dict in filtered_data]
-        date = [dict['dt_txt'] for dict in filtered_data]
-        # Graph part for temperature
-        figure = px.line(x=date, y=temperatures,
-                         labels={'x': 'Dates', 'y': 'Temperature (C)'})
-        st.plotly_chart(figure)
+try:
+    if place:
+        filtered_data = get_data(place, days)
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] for dict in filtered_data]
+            temperatures = [temp/10 for temp in temperatures]
+            date = [dict['dt_txt'] for dict in filtered_data]
+            # Graph part for temperature
+            figure = px.line(x=date, y=temperatures,
+                             labels={'x': 'Dates', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
 
-    if option == 'Sky':
-        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
-        images = {'Rain': 'images/rain.png', 'Clear': 'images/clear.png',
-                  'Clouds': 'images/cloud.png', 'Snow': 'images/snow.png'}
-        images_path = [images[conditions] for conditions in sky_conditions]
-        columns = st.columns(5)
-        for index, item in enumerate(images_path):
-            with columns[index % 5]:
-                text_1 = item.split('/')[1]
-                text_1 = text_1.split('.')[0]
-                st.image(item, width=115, caption=text_1)
+        if option == 'Sky':
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = {'Rain': 'images/rain.png', 'Clear': 'images/clear.png',
+                      'Clouds': 'images/cloud.png', 'Snow': 'images/snow.png'}
+            images_path = [images[conditions] for conditions in sky_conditions]
+            columns = st.columns(5)
+            for index, item in enumerate(images_path):
+                with columns[index % 5]:
+                    text_1 = item.split('/')[1]
+                    text_1 = text_1.split('.')[0]
+                    st.image(item, width=115, caption=text_1)
+
+except KeyError:
+    st.write('Please enter a valid city name')
 
 
